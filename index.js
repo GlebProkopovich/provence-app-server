320f55cabc96bb5fb7a8b4e1f6840bee6edcae80
Gleb Prokopovich <g.prokopovich@rambler.ru>
2023-06-03 00:39:29 +0400

    cors

--- index.js
+++ index.js
@@ -19,7 +19,22 @@ app.use(cookieParser());
 //     credentials: true,
 //   })
 // );
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://provence-app-client.vercel.app'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use('/api', router);
 app.use(errorMiddleware);
 
